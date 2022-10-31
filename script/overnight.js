13a42007c6fd423437c2fc192da6ad2b555629d4
AmyRobrigado <amyantanod@gmail.com>
2022-10-31 13:52:11 -0400

    Modified files

--- script/overnight.js
+++ script/overnight.js
@@ -2,3 +2,23 @@
 
 Window.onload = init;
 
+function init() {
+    //Room selection
+    const selectKng = document.getElementById("selectKng");
+    const selectQwn = document.getElementById("selectQwn");
+    const select2B = document.getElementById("select2B");
+
+    //date field 
+    const arrivalDate = document.getElementById("arrivalDate");
+    const departDate = document.getElementById("departDate");
+
+    //guest field
+    const guestNum1 = document.getElementById("guestNum1");
+    const guestNum2 = docuemnt.getElementById("guestNum2");
+
+    //Discount field
+    const selectDis = document.getElementById("selectDis");
+    
+    //Reserve Button
+    const resBtn = document.getElementById("resBtn");
+}
